f6d425ddfe3cf65ccdedb0dd2f9ca02d55bb664f
AaronvdAkker <tron2871@yahoo.com>
2016-04-23 01:19:06 +0200

    commit van 0105

---
+++ untitled/src/Testen/JavaTest.java
@@ -0,0 +1,10 @@
+package Testen;
+
+/**
+ * Created by Aaron on 23-4-2016.
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
